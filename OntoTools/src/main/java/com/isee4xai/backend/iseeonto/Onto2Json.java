1a0d896d758e96ef392f92a571f6a62cdf6fd896
jareciog <jareciog@fdi.ucm.es>
2022-01-31 20:19:22 +0100

    ontotools finished

--- OntoTools/src/main/java/com/isee4xai/backend/iseeonto/Onto2Json.java
+++ OntoTools/src/main/java/com/isee4xai/backend/iseeonto/Onto2Json.java
@@ -1,22 +1,19 @@
 package com.isee4xai.backend.iseeonto;
 
+import static org.semanticweb.owlapi.search.EntitySearcher.getAnnotationObjects;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.stream.Stream;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
-import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -25,7 +22,6 @@ import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
-import org.semanticweb.owlapi.search.EntitySearcher;
 
 /**
  * Hello world!
@@ -33,56 +29,49 @@ import org.semanticweb.owlapi.search.EntitySearcher;
  */
 public class Onto2Json 
 {
-    public static void main( String[] args ) throws FileNotFoundException
+	private static final String CLASS = "class";
+	private static final String INSTANCES2 = "instances";
+	private static final String SUBCLASSES = "subclasses";
+	private static final String NOTHING = "Nothing";
+	OWLOntology ontology;
+	
+	public static void main( String[] args ) throws FileNotFoundException
     {
+		Onto2Json parser = new Onto2Json();
+		parser.run();
+    }
+    
+    
+    
+	private void run() {
     	OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-    	OWLDataFactory df = OWLManager.getOWLDataFactory();
-    	OWLOntology ontology;
+    	//OWLDataFactory df = OWLManager.getOWLDataFactory();
+
     	OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
     	
 		
 		JSONObject jo = new JSONObject();
 		JSONArray ret = new JSONArray();
 		try {
-			jo.put("retrievalInfo",ret);
+			jo.put("iSeeOnto",ret);
 		} catch (JSONException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		try {
 			ontology = manager.loadOntologyFromOntologyDocument(new File("iSeeOnto.owl"));
-			OWLAnnotationProperty simAnno = df.getOWLAnnotationProperty("http://www.w3id.org/iSeeOnto/SimilarityKnowledge#applicableSimilarityStrategy");
-			for (Iterator<OWLClass> iterator = ontology.classesInSignature().iterator(); iterator.hasNext();) {
-				OWLClass cls = iterator.next();
-				Stream<OWLAnnotation> stream = EntitySearcher.getAnnotationObjects(cls, ontology);
-				Iterator<OWLAnnotation> it = stream.iterator();
-				while(it.hasNext())
-				{
-					OWLAnnotation anno = it.next();
-					if(anno.getProperty().equals(simAnno))
-					{
-						if(!anno.getValue().toString().equalsIgnoreCase("_:genid2147485010"))
-						{
-							JSONObject clase = new JSONObject();
-							clase.put(anno.getProperty().getIRI().getShortForm(), ((IRI) anno.getValue()).getShortForm());
-							//System.out.println(anno.getProperty().getIRI().getShortForm() + ": " + ((IRI) anno.getValue()).getShortForm());
-							writeToJSON( cls,  ontology,  reasonerFactory, clase);
-							ret.put(clase);
-	                     
-					
-						}
-					
-						
-					}
-						
-					
-				}
-			}
+			OWLClass top = manager.getOWLDataFactory().getOWLThing();
+			JSONObject clase = new JSONObject();
+			//clase.put(anno.getProperty().getIRI().getShortForm(), ((IRI) anno.getValue()).getShortForm());
+			//System.out.println(anno.getProperty().getIRI().getShortForm() + ": " + ((IRI) anno.getValue()).getShortForm());
+			writeToJSON( top,  ontology,  reasonerFactory, clase);
+			ret.put(clase);
+			
 
-				File f = new File("RetrievalInfo.json");
+				File f = new File("iSeeOnto.json");
 		    	FileWriter file = null;
 				f.createNewFile();
-				file = new FileWriter("RetrievalInfo.json", false);
+				file = new FileWriter("iSeeOnto.json", false);
 				file.write(jo.toString(2));
 				file.flush();
 				file.close();
@@ -99,14 +88,17 @@ public class Onto2Json
 			e.printStackTrace();
 		} 
     	
-    }
-    
-    
-    
-	static void writeToJSON(OWLClass cls, OWLOntology ontology, OWLReasonerFactory reasonerFactory,JSONObject clase) throws JSONException
+		
+	}
+
+
+
+	@SuppressWarnings("deprecation")
+	void writeToJSON(OWLClass cls, OWLOntology ontology, OWLReasonerFactory reasonerFactory,JSONObject clase) throws JSONException
     {
-		clase.put("class", cls.getIRI().getShortForm());
-		OWLReasoner reasoner = reasonerFactory.createReasoner(ontology);
+		//clase.put("class", cls.getIRI().getShortForm());
+		clase.put(CLASS, labelFor(cls));
+		OWLReasoner reasoner = reasonerFactory.createNonBufferingReasoner(ontology);
         NodeSet<OWLNamedIndividual> instances = reasoner.getInstances(cls, true);
         
         if(!instances.isEmpty())
@@ -117,35 +109,43 @@ public class Onto2Json
             {
             	instArray.put(it.next().getIRI().getShortForm());
             }
-            clase.put("instances", instArray);
+            clase.put(INSTANCES2, instArray);
            
             	
         }
     
         
-        Stream<OWLClassExpression> subs = EntitySearcher.getSubClasses(cls,ontology);
-        Iterator<OWLClassExpression> it = subs.iterator();
-        
-        if(it.hasNext())
-        {
-        	JSONArray subArray = new JSONArray();
-        	while(it.hasNext())
-        	{
-        		
-            	OWLClassExpression ce = it.next();
-            	JSONObject sub = new JSONObject();
-            	writeToJSON(ce.asOWLClass(), ontology, reasonerFactory, sub);
-            	subArray.put(sub);
-            	
-        	}
-        	clase.put("subclasses",subArray);
-        }
-        
-        
+       
+    	JSONArray subArray = new JSONArray();
+    	for (OWLClass child : reasoner.getSubClasses(cls, true).getFlattened()) 
+            if (!child.equals(cls) && !labelFor(child).equals(NOTHING)) {
+	        	JSONObject sub = new JSONObject();
+	        	writeToJSON(child, ontology, reasonerFactory, sub);
+	        	subArray.put(sub);
+    	}
+        if(!subArray.isEmpty())
+        	clase.put(SUBCLASSES,subArray);        	
+   
+          
     }
     
     
     
-    
+    private String labelFor(OWLClass clazz) {
+        /*
+         * Use a visitor to extract label annotations
+         */
+        LabelExtractor le = new LabelExtractor();
+        for (Object anno : getAnnotationObjects(clazz, ontology).toArray()) {
+            ((OWLAnnotation)anno).accept(le);
+        }
+        /* Print out the label if there is one. If not, just use the class URI */
+        if (le.getResult() != null) {
+            return le.getResult();
+        } else {
+        	String iri = clazz.getIRI().toString();
+            return iri.substring(iri.indexOf("#")+1);
+        }
+    }  
     
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 Repository for platform back-end
+
+## exchange-json-format
+
+Contains the schema and examples of the data exchange format
+
+
+## retrieval
+
+retrieval service
+
+## OntoTools
+
+Tools to transform the iSeeOnto.owl ontology to the required json format.
+
+* Onto2Json transforms the whole ontology to json. Resulting file is _iSeeOnto.json_
+* RetrievalInfo2Json only transforms those classes annotated with "applicableSimilarityStrategy". Resulting file is _RetrievalInfo.json_
