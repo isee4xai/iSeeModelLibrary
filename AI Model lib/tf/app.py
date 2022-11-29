3bbb1c05488279fd676edd98250ef939b8f187c4
Jesus Darias <71895708+jesusdarias@users.noreply.github.com>
2022-11-29 14:26:56 +0100

    Fix run methods

--- AI Model lib/tf/app.py
+++ AI Model lib/tf/app.py
@@ -2,21 +2,14 @@ import sys
 import pathlib
 from flask import Flask, send_from_directory,request, json, jsonify
 from flask_restful import Api
-from flask_restful import Resource,reqparse
-import matplotlib.pyplot
-import random
-import string
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 import markdown
 import markdown.extensions.fenced_code
-
 from PIL import Image
-
 import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask, flash, request
+
 
 UPLOAD_FOLDER = 'Models'
 ALLOWED_EXTENSIONS = {'h5'}
@@ -72,153 +65,166 @@ def output_file_html(data, code, headers):
     data["filename"],mimetype="text/html",as_attachment=True)
     return response
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+#def allowed_file(filename):
+#    return '.' in filename and \
+#           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def allowed_id(iden):
-    for c in NOT_ALLOWED_SYMBOLS:
-        if c in iden:
-            return False
-    return True
+#def allowed_id(iden):
+#    for c in NOT_ALLOWED_SYMBOLS:
+#        if c in iden:
+#            return False
+#    return True
 
-@app.route('/upload_model', methods=['POST', 'PUT'])
-def upload_model():
-    #Add a new model to the server
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        parameters = request.form.get('params')
-        if parameters is None:
-            flash('No params part')
-            return redirect(request.url)
-        parameters = json.loads(parameters)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')   
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            userid = request.form.get('id')
-            if userid is None or userid == '':
-                filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
-            else:
-                if allowed_id(userid):
-                    filename = userid
-                    if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], userid)):
-                        return 'A model with the id: ' + userid + ' already exists'
-                else:
-                    return 'The provided id is invalid'
-            pathlib.Path(app.config['UPLOAD_FOLDER'], filename).mkdir(exist_ok=True)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename, filename + EXTENSION))
-            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename ,filename + '.json'), 'w') as f:
-                json.dump(parameters, f)
-            return jsonify(
-                modelid = filename
-            )
-    #Update an existing model
-    elif request.method == 'PUT':
-        if 'file' not in request.files:
-            flash('No file part')
-            return 'A file hasnt been provided'
+#@app.route('/upload_model', methods=['POST', 'PUT'])
+#def upload_model():
+#    #Add a new model to the server
+#    if request.method == 'POST':
+#        # check if the post request has the file part
+#        if 'file' not in request.files:
+#            flash('No file part')
+#            return redirect(request.url)
+#        parameters = request.form.get('params')
+#        if parameters is None:
+#            flash('No params part')
+#            return redirect(request.url)
+#        parameters = json.loads(parameters)
+#        file = request.files['file']
+#        # If the user does not select a file, the browser submits an
+#        # empty file without a filename.
+#        if file.filename == '':
+#            flash('No selected file')   
+#            return redirect(request.url)
+#        if file and allowed_file(file.filename):
+#            userid = request.form.get('id')
+#            if userid is None or userid == '':
+#                filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
+#            else:
+#                if allowed_id(userid):
+#                    filename = userid
+#                    if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], userid)):
+#                        return 'A model with the id: ' + userid + ' already exists'
+#                else:
+#                    return 'The provided id is invalid'
+#            pathlib.Path(app.config['UPLOAD_FOLDER'], filename).mkdir(exist_ok=True)
+#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename, filename + EXTENSION))
+#            with open(os.path.join(app.config['UPLOAD_FOLDER'], filename ,filename + '.json'), 'w') as f:
+#                json.dump(parameters, f)
+#            return jsonify(
+#                modelid = filename
+#            )
+#    #Update an existing model
+#    elif request.method == 'PUT':
+#        if 'file' not in request.files:
+#            flash('No file part')
+#            return 'A file hasnt been provided'
 
-        iden = request.form.get('id')
-        if iden is None:
-            flash('No id provided')
-            return "An id field is needed in order to update the model"
+#        iden = request.form.get('id')
+#        if iden is None:
+#            flash('No id provided')
+#            return "An id field is needed in order to update the model"
 
-        parameters = request.form.get('params')
-        if parameters is None:
-            flash('No params part')
-            return 'A param field is needed in order to update the model'
-        file = request.files['file']
-        parameters = json.loads(parameters)
-        if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + EXTENSION))
-            with open(os.path.join(app.config['UPLOAD_FOLDER'], iden ,iden + '.json'), 'w') as f:
-                json.dump(parameters, f)
-            return "Model updated successfully"
-        else:
-            return "No model found with this id"
+#        parameters = request.form.get('params')
+#        if parameters is None:
+#            flash('No params part')
+#            return 'A param field is needed in order to update the model'
+#        file = request.files['file']
+#        parameters = json.loads(parameters)
+#        if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
+#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + EXTENSION))
+#            with open(os.path.join(app.config['UPLOAD_FOLDER'], iden ,iden + '.json'), 'w') as f:
+#                json.dump(parameters, f)
+#            return "Model updated successfully"
+#        else:
+#            return "No model found with this id"
 
-    return '''
-    The only supported actions for this request are POST and PUT
-    '''
+#    return '''
+#    The only supported actions for this request are POST and PUT
+#    '''
 
-@app.route('/dataset', methods=['POST', 'GET'])
-def dataset():   
-     if request.method == 'POST' :
-        iden = request.form.get('id')
-     elif request.method == 'GET':
-        iden = request.args.get('id')
-     else:
-        return "The only supported actions for this request are POST and GET"      
-     if iden is None:
-        flash('No params part')
-        return "The model id is missing"
-     if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
-         if request.method == 'POST' :
-            if 'file' not in request.files:
-                flash('No file part')
-                return "A file is missing"
-            file = request.files['file']
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.pkl'))
-            return "Dataset uploaded successfully"
-         elif request.method == 'GET' :
-            return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], iden), iden + '.pkl', as_attachment=True)
-         else:
-            return "The only supported actions for this request are POST and GET"
+#@app.route('/dataset', methods=['POST', 'GET'])
+#def dataset():   
+#     if request.method == 'POST' :
+#        iden = request.form.get('id')
+#     elif request.method == 'GET':
+#        iden = request.args.get('id')
+#     else:
+#        return "The only supported actions for this request are POST and GET"      
+#     if iden is None:
+#        flash('No params part')
+#        return "The model id is missing"
+#     if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
+#         if request.method == 'POST' :
+#            if 'file' not in request.files:
+#                flash('No file part')
+#                return "A file is missing"
+#            file = request.files['file']
+#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.pkl'))
+#            return "Dataset uploaded successfully"
+#         elif request.method == 'GET' :
+#            return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], iden), iden + '.pkl', as_attachment=True)
+#         else:
+#            return "The only supported actions for this request are POST and GET"
 
-     return "The model with the provided id doesn't exist"
+#     return "The model with the provided id doesn't exist"
 
-@app.route('/delete', methods=['DELETE'])
-def delete_model():
-    iden = request.form.get('id')
-    if iden is None:
-        flash('No params part')
-        return "The model id is missing"
-    if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
-        if request.method == 'DELETE':
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + EXTENSION))
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.json'))
-            if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.pkl'))):
-                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.pkl'))
-            os.rmdir(os.path.join(app.config['UPLOAD_FOLDER'], iden))
-            return "Model deleted successfully"
-        return "The only supported action for this request is DELETE"
-    return "The model does not exist"
+#@app.route('/delete', methods=['DELETE'])
+#def delete_model():
+#    iden = request.form.get('id')
+#    if iden is None:
+#        flash('No params part')
+#        return "The model id is missing"
+#    if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
+#        if request.method == 'DELETE':
+#            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + EXTENSION))
+#            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.json'))
+#            if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.pkl'))):
+#                os.remove(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + '.pkl'))
+#            os.rmdir(os.path.join(app.config['UPLOAD_FOLDER'], iden))
+#            return "Model deleted successfully"
+#        return "The only supported action for this request is DELETE"
+#    return "The model does not exist"
 
-@app.route('/info', methods=['GET'])
-def model_info():
-    iden = request.args.get('id')
-    if iden is None:
-        flash('No params part')
-        return "The model id is missing"
-    if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
-        if request.method == 'GET':
-            return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], iden), iden + '.json', as_attachment=True)
-        return "The only supported action for this request is GET"
-    return "The model does not exist"
+#@app.route('/info', methods=['GET'])
+#def model_info():
+#    iden = request.args.get('id')
+#    if iden is None:
+#        flash('No params part')
+#        return "The model id is missing"
+#    if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
+#        if request.method == 'GET':
+#            return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], iden), iden + '.json', as_attachment=True)
+#        return "The only supported action for this request is GET"
+#    return "The model does not exist"
 
 @app.route('/Image/run', methods=['POST'])
 def run_img_model():
     iden = request.form.get('id')
     if iden is None:
-        flash('No params part')
+        flash('No id part')
         return "The model id is missing"
     if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
         if request.method == 'POST':
             model = tf.keras.models.load_model(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + ".h5"), compile=False)
-            if 'image' not in request.files:
-                flash('No file part')
-                return "No image was provided"
-            image = request.files['image']
-            image = np.asarray(Image.open(image))
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + ".json")) as f:
+                model_info=json.load(f)
+            instance=request.form.get('instance')
+            if instance==None:
+                if 'image' not in request.files:
+                    flash('No image part')
+                    return "No image was provided"
+                image = request.files['image']
+                instance = np.asarray(Image.open(image))
+            else:
+                instance=np.asarray(json.loads(instance))
+            if instance.shape!=tuple(model_info["attributes"]["features"]["image"]["shape"]):
+                try:
+                    instance = instance.reshape(tuple(model_info["attributes"]["features"]["image"]["shape"]))
+                except:
+                    return "Cannot reshape image of shape " + str(instance.shape) + " into shape " + str(tuple(model_info["attributes"]["features"]["image"]["shape"]))
+            instance=instance.reshape((1,)+instance.shape)
+            print(instance.shape)
             try:
-                predictions = model.predict(image[None,:,:])
+                predictions = model.predict(instance)
                 return jsonify({'predictions' : predictions.tolist()})
             except Exception as e:
                 print(e)
@@ -238,9 +244,12 @@ def run_tab_model():
         if request.method == 'POST':
             model = tf.keras.models.load_model(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + ".h5"), compile=False)
             if instance is None:
-                flash('No file part')
-                return "No parameters were provided"
+                flash('No instance part')
+                return "No instances were provided"
             instance = json.loads(instance)
+            instance = np.asarray(instance)
+            if len(instance.shape)==1:
+                 instance=instance.reshape(1, -1)
             X = tf.convert_to_tensor(instance)
             try:
                 predictions = model.predict(X)
@@ -258,15 +267,20 @@ def run_text_model():
         flash('No params part')
         return "The model id is missing"
     if(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], iden))):
-        instance = request.form.get('instance')
+        data = request.form.get('instance')
         if request.method == 'POST':
             model = tf.keras.models.load_model(os.path.join(app.config['UPLOAD_FOLDER'], iden, iden + ".h5"), compile=False)
-            if instance is None:
-                flash('No file part')
-                return "No parameters were provided"
-            instance = json.loads(instance)
-            instance = instance['instance']
-            X = tf.convert_to_tensor(instance)
+            if data is None:
+                flash('No instances part')
+                return "No instances were provided"
+            X=data
+            try:
+                data=json.loads(data)
+                X = np.asarray(data)
+                if len(X.shape)==0:
+                    X=X.reshape(1, -1)
+            except:
+                X=[X]
             try:
                 predictions = model.predict(X)
                 return jsonify({'predictions' : predictions.tolist()})
@@ -277,4 +291,4 @@ def run_text_model():
     return "The model does not exist"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=4002)
